480c8585e16b9c00f74d5204fce92745c3a51695
Joana Catarino <joana.fa.catarino@gmail.com>
2024-07-17 09:41:32 +0200

    add chronometer to start counting with start button

---
+++ Task_GUI_Joana/chronometer_generator.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 16 11:24:48 2024
+
+@author: JoanaCatarino
+"""
+
+from PySide6.QtCore import QTimer, Signal, QObject
+
+class Chronometer(QObject):
+    timeChanged = Signal(str)
+
+    def __init__(self):
+        super().__init__()
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.updateTime)
+        self.elapsed_time = 0
+
+    def start(self):
+        self.elapsed_time = 0
+        self.timer.start(1000)
+
+    def stop(self):
+        self.timer.stop()
+
+    def updateTime(self):
+        self.elapsed_time += 1
+        hours = self.elapsed_time // 3600
+        minutes = (self.elapsed_time % 3600) // 60
+        seconds = self.elapsed_time % 60
+        self.timeChanged.emit(f"{hours:02} : {minutes:02} : {seconds:02}")
+
--- Task_GUI_Joana/taskController.py
+++ Task_GUI_Joana/taskController.py
@@ -1,9 +1,13 @@
 # This Python file uses the following encoding: utf-8
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
-from animal_id_generator import animal_id
+from PySide6.QtCore import QTimer, Slot
 from ui_form import Ui_TaskGui
 
+# Import different functions
+from animal_id_generator import animal_id
+from sound_generator import tone_10KHz, tone_5KHz, white_noise
+from chronometer_generator import Chronometer
 
 class TaskGui(QMainWindow):
     def __init__(self, parent=None):
@@ -14,16 +18,30 @@ class TaskGui(QMainWindow):
         # Call function that has the list of animals to add to the dropdown menu of Animal_ID   
         font_size = 10 # Font size of the items in the dropdown menu
         animal_id(self.ui.Box1_Animal_ID)
+        
+        # Initialize Chronometer
+        self.chronometer = Chronometer()
+        self.chronometer.timeChanged.connect(self.updateTime)
+        
+        self.ui.Box1_Start.clicked.connect(self.chronometer.start)
+        self.ui.Box1_Stop.clicked.connect(self.chronometer.stop)
+        
+    @Slot(str)
+    def updateTime(self, time_str):
+        self.ui.chronometer.setText(time_str)
 
-def test():
-    print('Hi world')
+#def test():
+   # print('Hi world')
  
         
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     widget = TaskGui()
     # Below is the custom function
-    widget.ui.Box1_Start.clicked.connect(test)
+    #widget.ui.Box1_Start.clicked.connect(test)
+    widget.ui.Box1_10Tone.clicked.connect(tone_10KHz)
+    widget.ui.Box1_5Tone.clicked.connect(tone_5KHz)
+    widget.ui.Box1_Punishment.clicked.connect(white_noise)
     widget.show()
     sys.exit(app.exec())
 
