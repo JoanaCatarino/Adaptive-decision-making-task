09da7aa0b9903f163b7a39f5dce6a417bd8bcc83
Joana Catarino <joana.fa.catarino@gmail.com>
2024-07-26 13:46:09 +0200

    update hardware file

--- Task_GUI_Joana/hardware.py
+++ Task_GUI_Joana/hardware.py
@@ -22,21 +22,12 @@ import asyncio
 led_blue = LED (22)
 led_red = LED(26)
 
-# Define actions for different commands 
-async def led_blue_action():
-    while True:
-        led.on()
-        sleep(1)
-        led.off()
-        sleep(1)
-
-
-'''
+# Define actions for different commands
 async def led_blue_action():
     led_blue.on()
     await asyncio.sleep(1)
     led_blue.off()    
-'''    
+
 async def led_red_action():
     led_red.on()
     await asyncio.sleep(1)
