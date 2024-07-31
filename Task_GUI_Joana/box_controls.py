b7ac6788f3313197af53e9b0a6dfecba59857a3e
Joana Catarino <joana.fa.catarino@gmail.com>
2024-07-31 10:23:51 +0200

    Updated BoxControls class with all the commands for Box1

--- Task_GUI_Joana/box_controls.py
+++ Task_GUI_Joana/box_controls.py
@@ -9,13 +9,37 @@ Created on Wed Jul 31 09:45:07 2024
 the gui
 
 """
-    
+import sys
+import asyncio
+import websockets
+from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtCore import QTimer, QDate, Slot
+from ui_form import Ui_TaskGui
+from threading import Thread
+from qasync import asyncSlot
+from qasync import QEventLoop, QApplication
+from server import Server
+
+# Import different functions/classes
+from animal_id_generator import animal_id
+from chronometer_generator import Chronometer
+from date_updater import DateUpdater
+from file_writer import write_task_start_file
+
+
 class BoxControls:
     
-    def __init__(self, ui, send_command_sync):
+    def __init__(self, ui, send_command_sync, update_time_slot):
         self.ui = ui
         self.send_command_sync = send_command_sync
+        self.update_time_slot = update_time_slot
+        self.current_task = None
+        
+        # Initialize components
         self._setup_controls()
+        self._setup_timer_and_chronometer()
+        self._populate_animal_id_dropdown()
+        self._connect_buttons()
 
     def _setup_controls(self):
         # Initially disable all buttons
@@ -31,6 +55,65 @@ class BoxControls:
         # Uncomment if needed
         # self.ui.Box1_Reward_right.clicked.connect(lambda: self.send_command_sync('reward_right'))
         # self.ui.Box1_Reward_left.clicked.connect(lambda: self.send_command_sync('reward_left'))
+
+    def _setup_timer_and_chronometer(self):
+        # Create and configure a DateUpdater
+        self.date_updater = DateUpdater(self.ui.Box1_Date, font_size=10)
+        
+        # Initialize Chronometer
+        self.Box1_Chronometer = Chronometer()
+        self.Box1_Chronometer.timeChanged.connect(self.update_time_slot)
+
+    def _populate_animal_id_dropdown(self):
+        # Populate the dropdown menu for Animal_ID
+        font_size = 10  # Font size of the items in the dropdown menu
+        animal_id(self.ui.Box1_Animal_ID)
+
+    def _connect_buttons(self):
+        # Connect Start and Stop buttons
+        self.ui.Box1_Start.clicked.connect(self.execute_task)
+        self.ui.Box1_Stop.clicked.connect(self.stop_task)
+
+    def execute_task(self):
+        # Stop any currently running task
+        self.stop_task()
+        
+        selected_task = self.ui.Box1_Task.currentText()
+        
+        # Create file with data unless the selected task is 'Test rig'
+        if selected_task != 'Test rig':
+            write_task_start_file(self.ui.Box1_Date, self.ui.Box1_Animal_ID)
+        
+        if selected_task == 'Test rig':
+            self.current_task = TestRig(self.ui)
+            self.enable_controls()
+        elif selected_task == 'Free Licking':
+            self.current_task = FreeLicking()
+        elif selected_task == 'Spout Sampling':
+            self.current_task = SpoutSampling()
+        elif selected_task == 'Two-Choice Auditory Task':
+            self.current_task = TwoChoiceAuditoryTask()
+        elif selected_task == 'Adaptive Sensorimotor Task':
+            self.current_task = AdaptiveSensorimotorTask()
+        elif selected_task == 'Adaptive Sensorimotor Task w/ Distractor':
+            self.current_task = AdaptiveSensorimotorTaskDistractor()
+            
+        if self.current_task:
+            self.current_task.start()
+            self.Box1_Chronometer.start()
+        else:
+            self.disable_controls()
+
+    def stop_task(self):
+        if self.current_task and hasattr(self.current_task, 'stop'):
+            self.current_task.stop()
+            self.current_task = None
+        
+        # Stop the chronometer 
+        self.Box1_Chronometer.stop()
+
+        # Disable test rig controls
+        self.disable_controls()
 
     def enable_controls(self):
         self.ui.Box1_BlueLED.setEnabled(True)
--- Task_GUI_Joana/taskController.py
+++ Task_GUI_Joana/taskController.py
@@ -16,20 +16,8 @@ from qasync import QEventLoop, QApplication
 from server import Server
 
 # Import different functions/classes
-from animal_id_generator import animal_id
-from sound_generator import tone_10KHz, tone_5KHz, white_noise
-from chronometer_generator import Chronometer
-from date_updater import DateUpdater
-from file_writer import write_task_start_file
 from box_controls import BoxControls
 
-# Import task classes
-from task_test_rig import TestRig
-from task_free_licking import FreeLicking
-from task_spout_sampling import SpoutSampling
-from task_twochoice_auditory import TwoChoiceAuditoryTask
-from task_adaptive_sensorimotor import AdaptiveSensorimotorTask
-from task_adaptive_sensorimotor_distractor import AdaptiveSensorimotorTaskDistractor
           
 
 class TaskGui(QMainWindow):
@@ -47,23 +35,8 @@ class TaskGui(QMainWindow):
                                                                 height: 14px;}''')
              
         
-        # Create and configure a Qtimer to have the current date in the gui
-        self.date_updater = DateUpdater(self.ui.Box1_Date, font_size = 10)
-        
-        # Call function that has the list of animals to add to the dropdown menu of Animal_ID   
-        font_size = 10 # Font size of the items in the dropdown menu
-        animal_id(self.ui.Box1_Animal_ID)
-        
-        # Initialize Chronometer
-        self.Box1_Chronometer = Chronometer()
-        self.Box1_Chronometer.timeChanged.connect(self.updateTime)
-        
-        # Connect Start button to the drop_down menu for the tasks and the Stop button to stop the task
-        self.ui.Box1_Start.clicked.connect(self.execute_task)
-        self.ui.Box1_Stop.clicked.connect(self.stop_task)
-        
         # Initialize BoxControls for Box 1
-        self.box1_controls = BoxControls(self.ui, self.send_command_sync(args, kwargs))
+        self.box1_controls = BoxControls(self.ui, self.send_command_sync)
         
         # Placeholder for the current task
         self.current_task = None
@@ -72,49 +45,7 @@ class TaskGui(QMainWindow):
         self.server = Server(self)
         loop = asyncio.get_event_loop()
         asyncio.run_coroutine_threadsafe(self.server.run(),loop)  
-
         
-    def execute_task(self):
-        # Stop any currently running task
-        self.stop_task()
-        
-        selected_task = self.ui.Box1_Task.currentText()
-        
-        # Create file with data unless the selected task is 'Test rig'
-        if selected_task != 'Test rig':
-            write_task_start_file(self.ui.Box1_Date, self.ui.Box1_Animal_ID)
-        
-        if selected_task == 'Test rig':
-            self.current_task = TestRig(self.ui)
-            self.enable_test_rig_controls()
-        elif selected_task == 'Free Licking':
-            self.current_task = FreeLicking()
-        elif selected_task == 'Spout Sampling':
-            self.current_task = SpoutSampling()
-        elif selected_task == 'Two-Choice Auditory Task':
-            self.current_task = TwoChoiceAuditoryTask()
-        elif selected_task == 'Adaptive Sensorimotor Task':
-            self.current_task = AdaptiveSensorimotorTask()
-        elif selected_task == 'Adaptive Sensorimotor Task w/ Distractor':
-            self.current_task = AdaptiveSensorimotorTaskDistractor()
-            
-        if self.current_task:
-            self.current_task.start()
-            self.Box1_Chronometer.start()
-        else:
-            self.box1_controls.disable_controls()
-        
-    def stop_task(self):
-        if self.current_task and hasattr(self.current_task, 'stop'):
-            self.current_task.stop()
-            self.current_task = None
-        
-        # Stop the chronometer 
-        self.ui.Box1_Stop.clicked.connect(self.Box1_Chronometer.stop)
-
-        # Disable test rig controls
-        self.box1_controls.disable_controls()
-
 
     # Define function to have the chonometer with the hour, minute and second as the text
     @Slot(str)
