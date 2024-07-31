6843629c3b40394d7231ec74e0dd2e6c89c0f23a
Joana Catarino <joana.fa.catarino@gmail.com>
2024-07-31 17:05:50 +0200

    await module test

--- Task_GUI_Joana/task_free_licking.py
+++ Task_GUI_Joana/task_free_licking.py
@@ -12,7 +12,7 @@ async def free_licking():
     quiet_window = 0 # pre-defined variable
     while True:
         print(f'Quiet window = {quiet_window}')
-        time.sleep(1)
+        await async.sleep(1)
 
 
 
