250613332adcf7c99223ba276007a44b51f942c5
Joana Catarino <joana.fa.catarino@gmail.com>
2024-10-04 15:20:19 +0200

    fix stylesheet

--- Task_GUI_Joana/stylesheet.py
+++ Task_GUI_Joana/stylesheet.py
@@ -14,7 +14,7 @@ from PyQt5.QtGui import QFont
 from PyQt5.QtCore import pyqtSlot, QTimer, QDate
 from form_updt import Ui_TaskGui
 
-def stylesheet(ui): 
+def stylesheet_colors(ui): 
     
     # Stylesheet to set colors to start, stop and update buttons
     ui.btn_Start.setStyleSheet('''QPushButton{ background-color:#85b79d;
@@ -44,15 +44,64 @@ def stylesheet(ui):
 
 # Style sheet to set style for disabled buttons                              
 
-    ui.setStyleSheet('''QRadioButton:disabled {color: gray;} 
-                        QRadioButton::indicator:disabled {border: 1px solid gray;
+    ui.chk_BlueLED.setStyleSheet('''QRadioButton:disabled {color: gray;} 
+                                    QRadioButton::indicator:disabled {border: 1px solid gray;
                                                                 background-color: transparent;
                                                                 border-radius: 7px;
                                                                 width: 14px;
-                                                                height: 14px;}
-                        QPushButton:disabled {color:gray;}''')
+                                                                height: 14px;}''')
+
+    ui.chk_WhiteLED_Left.setStyleSheet('''QRadioButton:disabled {color: gray;} 
+                                    QRadioButton::indicator:disabled {border: 1px solid gray;
+                                                                background-color: transparent;
+                                                                border-radius: 7px;
+                                                                width: 14px;
+                                                                height: 14px;}''')
+                                    
+    ui.chk_WhiteLED_Right.setStyleSheet('''QRadioButton:disabled {color: gray;} 
+                                    QRadioButton::indicator:disabled {border: 1px solid gray;
+                                                                background-color: transparent;
+                                                                border-radius: 7px;
+                                                                width: 14px;
+                                                                height: 14px;}''')
                         
+    ui.chk_Reward_Left.setStyleSheet('''QRadioButton:disabled {color: gray;} 
+                                    QRadioButton::indicator:disabled {border: 1px solid gray;
+                                                                background-color: transparent;
+                                                                border-radius: 7px;
+                                                                width: 14px;
+                                                                height: 14px;}''')
                         
+
+                               
+    ui.chk_Reward_Right.setStyleSheet('''QRadioButton:disabled {color: gray;} 
+                                    QRadioButton::indicator:disabled {border: 1px solid gray;
+                                                                background-color: transparent;
+                                                                border-radius: 7px;
+                                                                width: 14px;
+                                                                height: 14px;}''')
+
+    ui.chk_Punishment.setStyleSheet('''QRadioButton:disabled {color: gray;} 
+                                    QRadioButton::indicator:disabled {border: 1px solid gray;
+                                                                background-color: transparent;
+                                                                border-radius: 7px;
+                                                                width: 14px;
+                                                                height: 14px;}''')
+                                    
+    ui.chk_10Tone.setStyleSheet('''QRadioButton:disabled {color: gray;} 
+                                    QRadioButton::indicator:disabled {border: 1px solid gray;
+                                                                background-color: transparent;
+                                                                border-radius: 7px;
+                                                                width: 14px;
+                                                                height: 14px;}''')                                    
+
+    ui.chk_5Tone.setStyleSheet('''QRadioButton:disabled {color: gray;} 
+                                    QRadioButton::indicator:disabled {border: 1px solid gray;
+                                                                background-color: transparent;
+                                                                border-radius: 7px;
+                                                                width: 14px;
+                                                                height: 14px;}''')   
+
     ui.btn_Start.setStyleSheet('''QPushButton:disabled{ background-color:gray;
                                                     color: black;           
                                                     border: 0px solid gray; 
@@ -60,7 +109,6 @@ def stylesheet(ui):
                                                     padding: 1px;
                                                     font-size: 12px;
                                                     font-weight: bold}''')
-                        
                         
     ui.btn_Stop.setStyleSheet('''QPushButton:disabled{ background-color:gray;
                                                     color: black;           
