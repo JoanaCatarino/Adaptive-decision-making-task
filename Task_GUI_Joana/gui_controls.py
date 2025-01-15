bdfdb6be391fc5b43d68494c1a3689ceb84e6f63
Joana Catarino <joana.fa.catarino@gmail.com>
2025-01-15 12:00:53 +0100

    test

--- Task_GUI_Joana/gui_controls.py
+++ Task_GUI_Joana/gui_controls.py
@@ -305,7 +305,7 @@ class GuiControls:
             self.enable_controls()
 
         elif selected_task == 'Free Licking':
-            self.current_task = FreeLickingTask(self.ui)
+            self.current_task = FreeLickingTask(self)
 
 
         elif selected_task == 'Spout Sampling':
