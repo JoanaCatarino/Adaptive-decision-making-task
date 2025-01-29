c8844e18e4886a748709ada86afe2b2a95f806bf
Joana Catarino <joana.fa.catarino@gmail.com>
2025-01-29 16:10:10 +0100

    ..

--- Task_GUI_Joana/task_spout_sampling.py
+++ Task_GUI_Joana/task_spout_sampling.py
@@ -182,11 +182,10 @@ class SpoutSamplingTask:
                             self.total_licks += 1 # Implement total licks
                             self.licks_left +=1 # Implement licks left
                         
-                            #self.gui_controls.update_total_licks(self.total_licks) # Update the total trials in the GUI
-                            #self.gui_controls.update_licks_left(self.licks_left) # Update licks left in the GUI                            
+                            self.gui_controls.update_total_licks(self.total_licks) # Update the total trials in the GUI
+                            self.gui_controls.update_licks_left(self.licks_left) # Update licks left in the GUI                            
                             
-                        if elapsed_left > self.RW:
-                            self.tlast_lick = self.t
+
                             
             if self.piezo_reader.piezo_adder2:
                 latest_value2 = self.piezo_reader.piezo_adder2[-1]
@@ -215,11 +214,10 @@ class SpoutSamplingTask:
                             self.total_licks += 1 # Implement total licks
                             self.licks_right +=1 # Implement licks right
                         
-                            #self.gui_controls.update_total_licks(self.total_licks) # Update the total trials in the GUI
-                            #self.gui_controls.update_licks_right(self.licks_right) # Update licks right in the GUI     
+                            self.gui_controls.update_total_licks(self.total_licks) # Update the total trials in the GUI
+                            self.gui_controls.update_licks_right(self.licks_right) # Update licks right in the GUI     
                         
-                        elif elapsed_right > self.RW:
-                            self.tlast_lick = self.t
+
 
     
     def trial_has_started(self):
