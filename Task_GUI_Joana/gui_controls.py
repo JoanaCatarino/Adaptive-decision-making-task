1cd8ccbbc8240e89f8354e727b551a0a27259d4c
Joana Catarino <joana.fa.catarino@gmail.com>
2025-02-06 17:42:26 +0100

    bug fix

--- Task_GUI_Joana/gui_controls.py
+++ Task_GUI_Joana/gui_controls.py
@@ -28,7 +28,7 @@ from box_generator import box
 from task_generator import task
 from date_updater import DateUpdater
 from chronometer_generator import Chronometer
-from file_writer import write_task_start_file
+from file_writer import create_data_file
 from stylesheet import stylesheet
 from camera import start_camera, stop_camera, update_frame
 from piezo_plot import LivePlotWidget
