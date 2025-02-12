6c863608a5e0a3db203b72f5052264effc38a3b5
Joana Catarino <joana.fa.catarino@gmail.com>
2025-02-12 17:20:45 +0100

    test

--- Task_GUI_Joana/task_twochoice_auditory.py
+++ Task_GUI_Joana/task_twochoice_auditory.py
@@ -210,17 +210,25 @@ class TwoChoiceAuditoryTask:
             # 3. Play the sound cue and open response window
             print(f'Trial {trial_number}: Playing {self.current_tone} tone.')
             self.play_sound(self.current_tone)
-            self.detect_licks()
             
-            # 4. Determine Trial outcome
+            # RESPONSE WINDOW: Wait for a lick or timeout
+            trial_start_time = time.time()
+            while time.time() - trial_start_time < self.RW:
+                self.detect_licks()
+                if self.first_lick:
+                    break  # Stop waiting early if a lick is detected
+    
+            # AFTER RESPONSE WINDOW: Handle trial outcome
             if self.first_lick:
                 if self.first_lick == self.correct_spout:
-                    print(f'Trial {trial_number}: Correct choice! Delivering reward')
+                    print(f'Trial {trial_number}: Correct choice! Rewarding.')
                     self.reward(self.first_lick)
                 else:
-                    print(f'Trial {trial_number}: Incorrect choice! Delivering punishment')
-                    self.play_sound('white_noise') # Punishment
-                    time.sleep(2)
+                    print(f'Trial {trial_number}: Incorrect choice! Playing punishment sound.')
+                    self.play_sound('white_noise')
+                    time.sleep(2)  # Allow punishment duration
+            else:
+                print(f'Trial {trial_number}: No licks detected. Trial ending normally.')
                     
             self.light_off() # Turn off the light
             print(f'Trial {trial_number} ended')
