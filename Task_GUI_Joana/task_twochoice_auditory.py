e63859e60d70aa3aa00ab8e940750b1ac013d9e8
Joana Catarino <joana.fa.catarino@gmail.com>
2025-02-13 16:24:51 +0100

    bug fix

--- Task_GUI_Joana/task_twochoice_auditory.py
+++ Task_GUI_Joana/task_twochoice_auditory.py
@@ -211,7 +211,7 @@ class TwoChoiceAuditoryTask:
                     led_blue.off()
                     self.trialstarted = False
                     self.early_licks += 1
-                    self.gui_controls.update_early_licks(self.early_trials)
+                    self.gui_controls.update_early_licks(self.early_licks)
                     return 
             
             # 3. Play the sound 
