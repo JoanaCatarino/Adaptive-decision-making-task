17f0ae9dc229a6f5448d2bf0f231e398ff93d56e
Joana Catarino <joana.fa.catarino@gmail.com>
2025-02-19 14:16:21 +0100

    bug fix

--- Task_GUI_Joana/task_twochoice_auditory.py
+++ Task_GUI_Joana/task_twochoice_auditory.py
@@ -191,8 +191,8 @@ class TwoChoiceAuditoryTask:
         
         with self.lock:
             self.trialstarted = True
-            self.gui_controls.update_total_trials(self.total_trials)
             self.total_trials +1
+            self.gui_controls.update_total_trials(self.total_trials)
             self.ttrial = self.t # Update trial start time
             self.first_lick = None # Reset first lick at the start of each trial
             
