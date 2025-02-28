edf17355216154f5cca9ea6c88e208647897531d
Joana Catarino <joana.fa.catarino@gmail.com>
2025-02-28 14:37:08 +0100

    more tests

--- Task_GUI_Joana/task_twochoice_auditory.py
+++ Task_GUI_Joana/task_twochoice_auditory.py
@@ -69,7 +69,7 @@ class TwoChoiceAuditoryTask:
         self.prev_trialstarted = False
         self.first_trial = True
         self.early_lick_termination = False
-        autom_reward_termination = False
+        self.autom_reward_termination = False
         
         # Time variables
         self.tstart = None # start of the task
@@ -240,10 +240,11 @@ class TwoChoiceAuditoryTask:
                 threading.Thread(target=self.blue_led_off, daemon=True).start()
                 self.tend = time.time()
                 print(self.tend)
+                self.autom_reward_termination = True
     
                 # **Schedule next trial after ITI**
-                self.schedule_next_trial()
-                return  # Exit trial
+                #self.schedule_next_trial()
+                #return  # Exit trial
     
             else:  
                 # **If Automatic Reward is NOT checked, proceed with standard response window**
@@ -503,6 +504,15 @@ class TwoChoiceAuditoryTask:
                 if self.check_animal_quiet():
                     self.start_trial()
                     self.early_lick_termination = False
+                    self.ITI = round(random.uniform(3, 9), 1) # Set ITI for next trial
+                else:
+                    pass
+                
+            if self.autom_reward_termination and ((time.time() - (self.tend)) >= self.ITI) and not self.trialstarted:
+                print(f"ITI duration: {self.ITI} seconds")  # Print ITI value for debugging
+                if self.check_animal_quiet():
+                    self.start_trial()
+                    self.autom_reward_termination = False
                     self.ITI = round(random.uniform(3, 9), 1) # Set ITI for next trial
                 else:
                     pass
