a308fb1590e569bdec603ad893766b970fd9cfe9
Joana Catarino <joana.fa.catarino@gmail.com>
2025-03-05 14:01:04 +0100

    tests

--- Task_GUI_Joana/task_free_licking.py
+++ Task_GUI_Joana/task_free_licking.py
@@ -89,8 +89,6 @@ class FreeLickingTask:
         self.gui_controls.update_licks_left(0)
         self.gui_controls.update_licks_right(0)
         
-        self.gui_controls.reset_plot()
-        
         self.running = True
         self.tstart = time.time() # record the start time
         
