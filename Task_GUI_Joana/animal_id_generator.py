abfcf0e28b21ebc7524cd1df3efa7ae7618b0325
Joana Catarino <joana.fa.catarino@gmail.com>
2025-05-09 09:34:46 +0200

    new cohort of aniimals

--- Task_GUI_Joana/animal_id_generator.py
+++ Task_GUI_Joana/animal_id_generator.py
@@ -12,7 +12,7 @@ from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QComboBox
 from PyQt5.QtGui import QFont  # Import QFont for font manipulation
 
 def animal_id(combobox, font_size=8):
-    items = ["Test-animal","524345", "536455", "126846", "122364","344656", "000000"]
+    items = ["Test-animal","925145", "925146", "928012", "928013"]
     
     # Create a font with the specified size
     font = QFont()
