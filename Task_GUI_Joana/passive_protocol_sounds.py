c88b26242e150c5ec9aeeef5d9de828bcad14376
Joana Catarino <joana.fa.catarino@gmail.com>
2025-10-02 12:48:04 +0200

    tests

--- Task_GUI_Joana/passive_protocol_sounds.py
+++ Task_GUI_Joana/passive_protocol_sounds.py
@@ -32,59 +32,59 @@ class PassiveSoundRecordings:
         self.piezo_reader = gui_controls.piezo_reader
         
         
-   def start (self):
-       print ('Passive Protocol with Sounds starting')
-       
-       # Turn the LEDS ON initially
-       pump_l.on()
-       pump_r.on()
-       
-       self.running = True
-       
-       # Start main loop in a separate thread
-       self.print_thread = threading.Thread(target=self.main, daemon=True)
-       self.print_thread.start()   
-       
-       
-   def stop(self):
-       print("Stopping Adaptive Sensorimotor Task...")
-       
-       self.running = False
-           
-       pump_l.on()    
+    def start (self):
+        print ('Passive Protocol with Sounds starting')
         
+        # Turn the LEDS ON initially
+        pump_l.on()
+        pump_r.on()
+    
+        self.running = True
         
+        # Start main loop in a separate thread
+        self.print_thread = threading.Thread(target=self.main, daemon=True)
+        self.print_thread.start()   
+    
+    
+    def stop(self):
+        print("Stopping Adaptive Sensorimotor Task...")
+        
+        self.running = False
+            
+        pump_l.on()    
+         
+     
     def play_sound(self, frequency):
         if frequency == "8KHz":
             tone_8KHz()   # Your function for 8KHz sound
         elif frequency == "16KHz":
             tone_16KHz()  # Your function for 16KHz sound
             
-            
+         
     def run_sequence(self):
         # Play 8KHz 30 times, 5 sec apart
         for i in range(30):
             print(f"Playing 8KHz sound {i+1}/30")
             self.play_sound("8KHz")
             time.sleep(5)  # wait 5 sec before next sound
-
+       
         # Wait 10 sec before switching to 16KHz
         print("Waiting 10 seconds before 16KHz sequence...")
         time.sleep(10)
-
-        # Play 16KHz 30 times, 5 sec apart
-        for i in range(30):
-            print(f"Playing 16KHz sound {i+1}/30")
-            self.play_sound("16KHz")
-            time.sleep(5)
-            
-        print("Sequence of sounds is finished!")
-            
-        self.stop() # Stop protocol once all the sequence of sounds was played
+    
+         # Play 16KHz 30 times, 5 sec apart
+         for i in range(30):
+             print(f"Playing 16KHz sound {i+1}/30")
+             self.play_sound("16KHz")
+             time.sleep(5)
              
-        
-            
+         print("Sequence of sounds is finished!")
+             
+         self.stop() # Stop protocol once all the sequence of sounds was played
+              
+         
+         
     def main(self):
-        
+    
         while self.running:
             self.run_sequence()
