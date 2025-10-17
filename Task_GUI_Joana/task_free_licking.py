b189031aeaf8a2d0f13ac40116152970bb0db844
Joana Catarino <joana.fa.catarino@gmail.com>
2025-10-17 17:42:17 +0200

    test

--- Task_GUI_Joana/task_free_licking.py
+++ Task_GUI_Joana/task_free_licking.py
@@ -79,6 +79,8 @@ class FreeLickingTask:
         
         self.first_lick = None
         
+        self.trial_saved = False # added 15/05/2025
+        
 
     def start (self):
         print ('Free Licking task starting')
@@ -146,12 +148,15 @@ class FreeLickingTask:
         """ Initiates a trial, runs LED in parallel, and logs trial start"""
         
         with self.lock:
+            self.trial_saved = False # Added 15/05/2025
             self.trialstarted = True
             self.total_trials +=1
             self.gui_controls.update_total_trials(self.total_trials)
             self.ttrial = time.time() # Update trial start time
+            self.RW_start = time.time() # Start response window
             self.first_lick = None # Reset first lick at the start of each trial
             self.is_rewarded = False
+            self.data_saved = False
             
             print(f'Trial: {self.total_trials}')
             
@@ -201,8 +206,8 @@ class FreeLickingTask:
         # Response window in FreeLickingTask is [ttrial, ttrial+RW)
         if self.ttrial is None:
             return
-        in_rw_left  = (t_left  is not None) and (self.ttrial < t_left  < self.ttrial + self.RW)
-        in_rw_right = (t_right is not None) and (self.ttrial < t_right < self.ttrial + self.RW)
+        in_rw_left  = (t_left  is not None) and (self.ttrial < t_left  < self.RW_start + self.RW)
+        in_rw_right = (t_right is not None) and (self.ttrial < t_right < self.RW_start + self.RW)
     
         # Nothing to do or we already handled the first lick
         if not (in_rw_left or in_rw_right) or self.first_lick is not None:
