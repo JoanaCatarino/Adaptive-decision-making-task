c74f17bea26d5aec81e244217f8800b9b893a953
Joana Catarino <joana.fa.catarino@gmail.com>
2025-10-24 17:11:59 +0200

    test

--- Task_GUI_Joana/task_twochoice_auditory_blocks.py
+++ Task_GUI_Joana/task_twochoice_auditory_blocks.py
@@ -578,6 +578,7 @@ class TwoChoiceAuditoryTask_Blocks:
         trial_data = [
             np.nan if not hasattr(self, 'total_trials') else self.total_trials,  # trial number
             np.nan if not hasattr(self, 'ttrial') else self.ttrial,  # trial start
+            np.nan if not hasattr(self, 'RW_start') else self.RW_start, # Response window start
             np.nan if not hasattr(self, 'tend') else self.tend,  # trial end
             np.nan if not hasattr(self, 'trial_duration') else self.trial_duration,  # trial duration
             np.nan if not hasattr(self, 'ITI') else self.ITI,  # ITI
@@ -586,7 +587,6 @@ class TwoChoiceAuditoryTask_Blocks:
             np.nan if not hasattr(self, 'sound_played') else (1 if self.sound_played else 0),  # stim
             np.nan if not hasattr(self, 'current_tone') else (1 if self.current_tone == '8KHz' else 0),  # 5KHz
             np.nan if not hasattr(self, 'current_tone') else (1 if self.current_tone == '16KHz' else 0),  # 16KHz
-            np.nan if not hasattr(self, 'RW_start') else self.RW_start, # Response window start
             np.nan if not hasattr(self, 'first_lick') else (1 if self.first_lick else 0),  # lick
             np.nan if not hasattr(self, 'first_lick') else (1 if self.first_lick == 'left' else 0),  # left spout
             np.nan if not hasattr(self, 'first_lick') else (1 if self.first_lick == 'right' else 0),  # right spout
