da6b558939131af274bd2f3ffc46703cfb699ace
Joana Catarino <joana.fa.catarino@gmail.com>
2026-09-14 08:26:25 +0200

    new cohort of animals

--- Task_GUI_Joana/animal_id_generator.py
+++ Task_GUI_Joana/animal_id_generator.py
@@ -12,7 +12,7 @@ from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QComboBox
 from PyQt5.QtGui import QFont  # Import QFont for font manipulation
 
 def animal_id(combobox, font_size=8):
-    items = ["Test-animal", "1061768", "1060358", "1060359", "1060360", "1060148", "1053835", "1053836", "1053833", "1060138", "1060141", "1061220", "1061221", "1061233", "1061234"]
+    items = ["Test-animal", "1105695", "1105696", "1107081", "1107080", "1105692", "1105693", "1112090", "1114859"]
     
     # Create a font with the specified size
     font = QFont()
--- Task_GUI_Joana/spout_tone_generator.py
+++ Task_GUI_Joana/spout_tone_generator.py
@@ -19,7 +19,7 @@ spouts = ['left', 'right']
 tones = ['8KHz', '16KHz']
 
 # List of animals that need assigment of pairs
-animals = ["1061768", "1060358", "1060359", "1060360", "1060148", "1053835", "1053836", "1053833", "1060138", "1060141", "1061220", "1061221", "1061233", "1061234"]
+animals = ["1105695", "1105696", "1107081", "1107080", "1105692", "1105693", "1112090", "1114859"]
 
 # Directory and filename for csv with assignments
 directory = 'L:/dmclab/Joana/PFC-Str_behavior_project/Spout-tone map'
